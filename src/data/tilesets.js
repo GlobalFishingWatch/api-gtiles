506ff60921d00d8bb57a0b7a5ab4aac852b5db19
Erik Escoffier <erik.escoffier@gmail.com>
2019-07-18 11:57:04 +0200

    North Star style update
    This follows an issue noticed by @satellitestudiodesign on the North Star basemap style (natural parks appeared in an ugly brown)

--- src/data/tilesets.js
+++ src/data/tilesets.js
@@ -15,316 +15,120 @@ module.exports = {
     highDpi: true,
     styles: [
       {
-        elementType: "geometry",
-        stylers: [
+        "elementType": "geometry",
+        "stylers": [
           {
-            color: "#001436"
+            "color": "#001436"
           }
         ]
       },
       {
-        elementType: "labels",
-        stylers: [
+        "elementType": "labels",
+        "stylers": [
           {
-            visibility: "off"
+            "visibility": "off"
           }
         ]
       },
       {
-        elementType: "labels.icon",
-        stylers: [
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
           {
-            visibility: "off"
+            "color": "#153d7e"
           }
         ]
       },
       {
-        elementType: "labels.text.fill",
-        stylers: [
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [
           {
-            color: "#757575"
+            "color": "#153d7e"
+          },
+          {
+            "weight": 1.5
           }
         ]
       },
       {
-        elementType: "labels.text.stroke",
-        stylers: [
+        "featureType": "administrative.province",
+        "elementType": "geometry.stroke",
+        "stylers": [
           {
-            color: "#212121"
+            "color": "#153d7e"
+          },
+          {
+            "weight": 1
           }
         ]
       },
       {
-        featureType: "administrative",
-        elementType: "geometry",
-        stylers: [
+        "featureType": "poi",
+        "stylers": [
           {
-            color: "#757575"
+            "visibility": "off"
           }
         ]
       },
       {
-        featureType: "administrative.country",
-        elementType: "geometry.stroke",
-        stylers: [
+        "featureType": "road",
+        "stylers": [
           {
-            color: "#153d7e"
+            "visibility": "off"
           }
         ]
       },
       {
-        featureType: "administrative.country",
-        elementType: "labels.text.fill",
-        stylers: [
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
           {
-            color: "#85bbd5"
+            "visibility": "off"
           }
         ]
       },
       {
-        featureType: "administrative.country",
-        elementType: "labels.text.stroke",
-        stylers: [
+        "featureType": "transit",
+        "stylers": [
           {
-            color: "#001538"
+            "visibility": "off"
           }
         ]
       },
       {
-        featureType: "administrative.land_parcel",
-        stylers: [
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
           {
-            visibility: "off"
+            "color": "#757575"
           }
         ]
       },
       {
-        featureType: "administrative.locality",
-        elementType: "geometry.stroke",
-        stylers: [
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
           {
-            color: "#153d7e"
+            "color": "#000000"
           }
         ]
       },
       {
-        featureType: "administrative.locality",
-        elementType: "labels.text.fill",
-        stylers: [
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
           {
-            color: "#85bbd5"
+            "color": "#00265c"
           }
         ]
       },
       {
-        featureType: "administrative.locality",
-        elementType: "labels.text.stroke",
-        stylers: [
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
           {
-            color: "#001538"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.neighborhood",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.province",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#153d7e"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.province",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#85bbd5"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.province",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#001538"
-          }
-        ]
-      },
-      {
-        featureType: "landscape.natural",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#8abbc7"
-          }
-        ]
-      },
-      {
-        featureType: "landscape.natural",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#001538"
-          }
-        ]
-      },
-      {
-        featureType: "poi",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#757575"
-          }
-        ]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#181818"
-          }
-        ]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#616161"
-          }
-        ]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#1b1b1b"
-          }
-        ]
-      },
-      {
-        featureType: "road",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#2c2c2c"
-          }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#8a8a8a"
-          }
-        ]
-      },
-      {
-        featureType: "road.arterial",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#373737"
-          }
-        ]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#3c3c3c"
-          }
-        ]
-      },
-      {
-        featureType: "road.highway.controlled_access",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#4e4e4e"
-          }
-        ]
-      },
-      {
-        featureType: "road.local",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#616161"
-          }
-        ]
-      },
-      {
-        featureType: "transit",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      },
-      {
-        featureType: "transit",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#757575"
-          }
-        ]
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#000000"
-          }
-        ]
-      },
-      {
-        featureType: "water",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#00265c"
-          }
-        ]
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#0088cc"
+            "color": "#0088cc"
           }
         ]
       }
