cd65251e70f3f9f63e908d70d57ad5d16d496bfb
Raul <rrequero@gmail.com>
2025-04-08 09:04:45 +0200

    add referer

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || 'development';
 
 const errorMessage = (key, doc) =>
   `You need to configure the environment variable ${key}. ${doc}`;
@@ -26,68 +26,74 @@ module.exports = {
 
   admin: {
     token: entry({
-      key: "ADMIN_TOKEN",
-      doc: "Simple token to authenticate admin requests to the service",
+      key: 'ADMIN_TOKEN',
+      doc: 'Simple token to authenticate admin requests to the service',
       required: true,
-      defaults: { "development": "password" },
+      defaults: { development: 'password' },
     }),
   },
 
   google: {
     maps: {
       apiKey: entry({
-        key: "GOOGLE_MAPS_API_KEY",
-        doc: "Google Maps Tiles API key",
-        required: true
-      })
-    }
+        key: 'GOOGLE_MAPS_API_KEY',
+        doc: 'Google Maps Tiles API key',
+        required: true,
+      }),
+      referer: entry({
+        key: 'GOOGLE_MAPS_API_REFERER',
+        doc: 'Google Maps Tiles API referer',
+        required: false,
+        defaults: { all: 'https://gtiles.globalfishingwatch.org/' },
+      }),
+    },
   },
 
   redis: {
     connectionString: entry({
-      key: "REDIS_CONNECTION_STRING",
-      doc: "Connection string to the redis server",
-      required: true
+      key: 'REDIS_CONNECTION_STRING',
+      doc: 'Connection string to the redis server',
+      required: true,
     }),
 
     namespace: entry({
-      key: "REDIS_NAMESPACE",
-      doc: "Prefix for redis keys to namespace entries",
-      defaults: { development: "api-gtiles" },
-      required: true
-    })
+      key: 'REDIS_NAMESPACE',
+      doc: 'Prefix for redis keys to namespace entries',
+      defaults: { development: 'api-gtiles' },
+      required: true,
+    }),
   },
 
   server: {
     host: entry({
-      key: "SERVER_HOST",
-      doc: "Protocol, host and port where the server is exposed to clients.",
-      defaults: { development: "http://localhost:8080" },
-      required: false
+      key: 'SERVER_HOST',
+      doc: 'Protocol, host and port where the server is exposed to clients.',
+      defaults: { development: 'http://localhost:8080' },
+      required: false,
     }),
 
     port: entry({
-      key: "PORT",
-      doc: "Port on which the server is exposed to clients.",
+      key: 'PORT',
+      doc: 'Port on which the server is exposed to clients.',
       defaults: { all: 8080 },
-      required: false
+      required: false,
     }),
 
     protocol: entry({
-      key: "SERVER_PROTOCOL",
-      doc: "Protocol by which the server is exposed to clients.",
-      defaults: { development: "http", production: "https" },
-      required: false
-    })
+      key: 'SERVER_PROTOCOL',
+      doc: 'Protocol by which the server is exposed to clients.',
+      defaults: { development: 'http', production: 'https' },
+      required: false,
+    }),
   },
 
   log: {
     level: entry({
-      key: "LOG_LEVEL",
+      key: 'LOG_LEVEL',
       doc:
-        "Logging level. In increasing amount of logs: error, warn, info, verbose, debug, silly",
-      defaults: { development: "debug", production: "warn" },
-      required: true
-    })
+        'Logging level. In increasing amount of logs: error, warn, info, verbose, debug, silly',
+      defaults: { development: 'debug', production: 'warn' },
+      required: true,
+    }),
   },
 };
